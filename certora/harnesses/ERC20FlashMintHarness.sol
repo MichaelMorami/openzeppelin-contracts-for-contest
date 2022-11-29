a89f2b4400d317d64af48f15a4b45c4c744ef251
Michael M <MichaelM@certora.com>
2022-11-29 16:57:49 +0200

    cleaning tree and adding make bug

--- certora/harnesses/ERC20FlashMintHarness.sol
+++
@@ -1,5 +0,0 @@
-import "../munged/token/ERC20/extensions/ERC20FlashMint.sol";
-
-contract ERC20FlashMintHarness is ERC20FlashMint {
-    constructor(string memory name, string memory symbol) ERC20(name, symbol) {}
-}
--- certora/scripts/passes/verifyERC20Wrapper.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -euxo pipefail
-
-certoraRun \
-    certora/harnesses/ERC20WrapperHarness.sol certora/harnesses/ERC20PermitHarness.sol \
-    --verify ERC20WrapperHarness:certora/specs/ERC20Wrapper.spec \
-    --optimistic_loop \
-    $@
