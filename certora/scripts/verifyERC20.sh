8441b7c3b3ecb5f091aa630233747e64204ec495
Michael M <MichaelM@certora.com>
2022-11-29 18:51:37 +0200

    adding erc20methods and fixing script

--- certora/scripts/verifyERC20.sh
+++ certora/scripts/verifyERC20.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 
-set -euxo pipefail
-
 certoraRun \
     certora/harnesses/ERC20PermitHarness.sol \
     --verify ERC20PermitHarness:certora/specs/ERC20.spec \
-    --solc solc \
+    --solc solc8.17 \
     --optimistic_loop \
     $@
