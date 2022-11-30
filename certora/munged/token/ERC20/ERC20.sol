6d2388dc762668220ce82f96a2d3a994eb8db85f
Michael M <MichaelM@certora.com>
2022-11-30 18:43:06 +0200

    making chage in munged

--- certora/munged/token/ERC20/ERC20.sol
+++ certora/munged/token/ERC20/ERC20.sol
@@ -337,7 +337,7 @@ contract ERC20 is Context, IERC20, IERC20Metadata {
         address owner,
         address spender,
         uint256 amount
-    ) internal virtual {
+    ) public virtual { // i changed internal to public
         uint256 currentAllowance = allowance(owner, spender);
         if (currentAllowance != type(uint256).max) {
             require(currentAllowance >= amount, "ERC20: insufficient allowance");
