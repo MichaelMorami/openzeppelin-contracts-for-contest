5cbe7dec8649a33ed5892313bbe508aa86d48cb8
Michael M <MichaelM@certora.com>
2022-12-01 15:38:39 +0200

    restoring to template

--- certora/scripts/verify_ERC20_issues.sh
+++ certora/scripts/verify_ERC20_issues.sh
@@ -4,7 +4,7 @@ set -euxo pipefail
 
 certoraRun \
     certora/harnesses/ERC20PermitHarness.sol \
-    --verify ERC20PermitHarness:certora/specs/ERC20.spec \
+    --verify ERC20PermitHarness:certora/specs/ERC20_issues.spec \
     --solc solc \
     --optimistic_loop \
     $@
---
+++ certora/scripts/verify_ERC20_verified.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+certoraRun \
+    certora/harnesses/ERC20PermitHarness.sol \
+    --verify ERC20PermitHarness:certora/specs/ERC20_verified.spec \
+    --solc solc \
+    --optimistic_loop \
+    $@
