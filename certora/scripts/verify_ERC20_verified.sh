5d6770e089efdd3fc1c95ba179245a4b1f00eabb
Michael M <MichaelM@certora.com>
2022-12-01 16:25:08 +0200

    adding cloud flag to script

--- certora/scripts/verify_ERC20_verified.sh
+++ certora/scripts/verify_ERC20_verified.sh
@@ -6,5 +6,6 @@ certoraRun \
     certora/harnesses/ERC20PermitHarness.sol \
     --verify ERC20PermitHarness:certora/specs/ERC20_verified.spec \
     --solc solc \
+    --cloud \
     --optimistic_loop \
     $@
