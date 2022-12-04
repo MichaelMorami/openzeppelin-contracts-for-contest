0cc4333187356c004d1b2475233b5d9f1b512c07
Michael M <MichaelM@certora.com>
2022-12-04 20:39:29 +0200

    reverting to the original files

--- certora/scripts/verify_ERC20_issues.sh
+++ certora/scripts/verify_ERC20_issues.sh
@@ -3,7 +3,7 @@
 certoraRun \
     certora/harnesses/ERC20PermitHarness.sol \
     --verify ERC20PermitHarness:certora/specs/ERC20_issues.spec \
-    --solc solc \
+    --solc solc8.17 \
     --cloud \
     --optimistic_loop \
     $@
--- certora/scripts/verify_ERC20_verified.sh
+++ certora/scripts/verify_ERC20_verified.sh
@@ -3,8 +3,7 @@
 certoraRun \
     certora/harnesses/ERC20PermitHarness.sol \
     --verify ERC20PermitHarness:certora/specs/ERC20_verified.spec \
-    --solc solc \
+    --solc solc8.17 \
     --cloud \
     --optimistic_loop \
     $@
-#change
